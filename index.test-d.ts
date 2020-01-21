abb22e3f4385f4a80446433d485ebd25711ae301
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-21 18:32:35 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,12 +3,12 @@ import {CLIEngine} from 'eslint';
 import format = require('.');
 
 const result: CLIEngine.LintResult = {
-    filePath: 'fake-file.js',
-    errorCount: 0,
-    warningCount: 0,
-    messages: [],
-    fixableErrorCount: 0,
-    fixableWarningCount: 0
+	filePath: 'fake-file.js',
+	errorCount: 0,
+	warningCount: 0,
+	messages: [],
+	fixableErrorCount: 0,
+	fixableWarningCount: 0
 };
 
 expectType<string>(format([result]));
