b45c31fdc020664e5a58fd648cb5427e2694d7f5
Sindre Sorhus <sindresorhus@gmail.com>
2021-06-05 21:55:35 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,12 +3,12 @@ import type {ESLint, Linter} from 'eslint';
 export = formatterPretty;
 
 /**
- * Pretty formatter for [ESLint](https://eslint.org).
- *
- * @param results - Lint result for the individual files.
- * @param data - Extended information related to the analysis results.
- * @returns The formatted output.
- */
+Pretty formatter for [ESLint](https://eslint.org).
+
+@param results - Lint result for the individual files.
+@param data - Extended information related to the analysis results.
+@returns The formatted output.
+*/
 declare function formatterPretty(
 	results: readonly formatterPretty.LintResult[],
 	data?: ESLint.LintResultData
